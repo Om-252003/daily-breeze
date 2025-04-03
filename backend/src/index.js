8d6f91fafc46b3ba44cc465febdc2c1dffa4dcee
Omsai Alladwar <omsaialladwar@gmail.com>
2025-04-03 09:32:18 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,9 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 // to remove cors error
-app.use(cors());
+// app.use(cors());
 header ('Access-Control-Allow-Origin: *');	
 header ('Access-Control-ALlow-Methods: POST, GET, OPTIONS, PUT, DELETE');	
 header ('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');	
